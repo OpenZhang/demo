ee498cd38a5eb936f3ef834e26155de61811adfb
zhangkai <571073038@qq.com>
2019-04-09 15:21:19 +0800

    aaa

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @RequestMapping("/hello")
     public Object sayHello() {
-        return "hello-13!";
+        return "hello-13aaaaa!";
     }
 }
 
